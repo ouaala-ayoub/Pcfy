0bcc6caf219ea60397529428faae57328c541efa
ouaala-ayoub <ouaalaayoub12@gmail.com>
2022-09-13 00:13:49 +0000

    starting to implement permission checker

--- app/src/main/java/com/example/pc/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/pc/ui/activities/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.pc.ui.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.pc.R
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,45 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         supportActionBar?.hide()
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Log.i(TAG, "onCreate: permission granted : $isGranted")
+                } else {
+                    Log.i(TAG, "onCreate: permission granted : $isGranted")
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                    finish()
+                }
+            }
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                Log.i(TAG, "permission is aleardy granted: ")
+            }
+            //            shouldShowRequestPermissionRationale(...) -> {
+            //            // In an educational UI, explain to the user why your app requires this
+            //            // permission for a specific feature to behave as expected. In this UI,
+            //            // include a "cancel" or "no thanks" button that allows the user to
+            //            // continue using your app without granting the permission.
+            //            showInContextUI(...)
+            //        }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
     }
 
     override fun onBackPressed() {
